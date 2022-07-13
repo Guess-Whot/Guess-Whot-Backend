6798799ac92168e7e3098ef5bbabca9e0ba78599
Alex Orlet <AlexOrlet89@gmail.com>
2022-07-13 12:53:12 -0700

    current user testing, needs 2 new control routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,23 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'test@example.com',
   password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('User Testing', () => {
@@ -21,6 +34,13 @@ describe('User Testing', () => {
       id: expect.any(String),
       email,
     });
+  });
+
+  it('returns the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const res = await agent.get('/api/v1/users/me');
+    expect(res.status).toEqual(200);
+    console.log(res.body);
   });
   afterAll(() => {
     pool.end();
