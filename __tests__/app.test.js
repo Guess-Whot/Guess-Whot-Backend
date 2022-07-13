e9dd7abc1ca7c30ae953894921dad90708bf13e6
Alex Orlet <AlexOrlet89@gmail.com>
2022-07-13 12:27:37 -0700

    user testing for creating new user, returns 404

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,24 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const mockUser = {
+  email: 'test@example.com',
+  password: '123456',
+};
+
+describe('User Testing', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('Creates a new User', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+    expect(res.status).toEqual(200);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
   afterAll(() => {
     pool.end();
