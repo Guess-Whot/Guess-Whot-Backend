eac4ec86e43d58bb726e95fd43ee24a4ea1de528
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-07-14 12:13:58 -0700

    small changes

--- __tests__/characters.test.js
+++ __tests__/characters.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend animal routes', () => {
+describe('backend character routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
