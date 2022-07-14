ae7f7d9c593ef27011a1e7bc79841cb97e223472
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-07-14 11:37:09 -0700

    added characters test and characters route in app.js

---
+++ __tests__/characters.test.js
@@ -0,0 +1,20 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend animal routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('Get /characters displays all characters', async () => {
+    const resp = await request(app).get('/api/v1/characters');
+    expect(resp.body.length).toEqual(25);
+    expect(resp.body[0].name).toEqual('Fry');
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
