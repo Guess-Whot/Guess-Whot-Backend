328bbac05f02bd4bbe82eecfe1a0f5f89897a906
BrendenSeifried <brendenseifried@gmail.com>
2022-07-15 16:15:04 -0700

    flipped_recieved  socket now hooked up correctly

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -27,7 +27,6 @@ describe('User Testing', () => {
   });
   it('Creates a new User', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
-    // const { email } = mockUser;
     expect(res.status).toEqual(200);
 
     expect(res.body).toEqual({
